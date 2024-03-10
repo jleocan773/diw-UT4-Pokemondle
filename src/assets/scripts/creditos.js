2deade0997ae693fa7717674b55d1b411d3622d7
jleocan773 <115530217+jleocan773@users.noreply.github.com>
2024-03-10 21:35:39 +0100

    JLC | 2024-03-10 | Pokémondle - Elemento Interactivo Añádido - Acordeón

---
+++ src/assets/scripts/creditos.js
@@ -0,0 +1,47 @@
+//Función para activar y desactivar elementos al hacer clic en ellos
+
+function onIniciar() {
+  const accesibilidadIcon = document.querySelector(".accesibilidad");
+
+  //Creamos el evento en caso de que se le haga clic al icono
+  accesibilidadIcon.addEventListener("click", function () {
+    //Cambiamos la fuente del body
+    body.style.fontFamily = "'OpenDyslexic3-Regular', Arial, sans-serif";
+    body.style.fontSize = "0.8em";
+  });
+
+  const hora = new Date().getHours();
+  const body = document.querySelector("body");
+
+  if (hora >= 6 && hora < 22) {
+    //Si la hora es mayor o igual que las 6 de la mañana pero menor que las 6 de la tarde se muestra el fondo de día
+    body.classList.add("fondoDia");
+  } else {
+    //De  lo contrario, se muestra el fondo de noche
+    body.classList.add("fondoNoche");
+    body.style.color = "#FECA04";
+  }
+
+  //Código del Acordeón
+  //1º Seleccionamos todos los elementos sobre los que queremos trabajar
+  //h2Titulo es sobre el que haremos clic
+  const h2Titulo = document.querySelectorAll(".divContenedor__titulo");
+  //pContenido es el que modificaremos
+  const pContenido = document.querySelectorAll(".divContenedor__contenido");
+
+  h2Titulo.forEach((nodo, i) => {
+    nodo.addEventListener("click", () => {
+      if (
+        pContenido[i].classList.contains("divContenedor__contenido--activo")
+      ) {
+        pContenido[i].classList.remove("divContenedor__contenido--activo");
+        pContenido[i].querySelector(".imagenContenido").style.display = "none"; // Oculta la imagen
+      } else {
+        pContenido[i].classList.add("divContenedor__contenido--activo");
+        pContenido[i].querySelector(".imagenContenido").style.display = "block"; // Muestra la imagen
+      }
+    });
+  });
+}
+
+window.onload = onIniciar();
